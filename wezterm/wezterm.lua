39eecb8c7074d76af954b271c32d218b12271c10
SekineIori <iori20020520@icloud.com>
2026-01-03 00:13:29 +0900

    first

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,79 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+--keybinds
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
+config.leader = {key = "q",mods = "CTRL",timeout_milliseconds = 2000}
+--basicconfig
+-- Changing the default program: wsl
+config.default_prog = { "wsl.exe", "--distribution", "ubuntu", "--cd", "~" }
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+--config.prefer_to_use_dark_theme = true
+
+--透過設定
+config.window_background_opacity = 0.8
+--config.win32_system_backdrop = 'Acrylic'
+config.window_decorations = 'RESIZE'
+
+--
+config.show_tabs_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+
+config.window_frame = {
+    inactive_titlebar_bg = "none",
+    active_titlebar_bg = "none",
+}
+
+config.window_background_gradient = {
+    colors = { "#000000" }
+}
+
+--タブバーのボタン関係
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.colors = {
+    tab_bar ={
+        inactive_tab_edge = "none"
+    }
+}
+
+--nerdfontsの→
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+local background = "#5c6d74"
+local foreground = "#FFFFFF"
+local edge_background = "none"
+
+if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+end
+
+local edge_foreground = background
+local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+return {
+    --左側の矢印
+    { Background = { Color =  edge_background} },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    --タブのテキスト部分
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    --右側の矢印
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+}
+end)
+
+return config
